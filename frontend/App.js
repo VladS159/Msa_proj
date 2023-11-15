342a3663c9015fc4747bd489bf442d5daeb2a12f
Mnv22 <mnv22081@gmail.com>
2023-11-15 23:34:20 +0200

    Added buttons.

--- frontend/App.js
+++ frontend/App.js
@@ -4,13 +4,38 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>:)</Text>
+      <text>Monkey Business</text>
+      <form>
+        <label>
+          User:
+          <input type="text" name="user" />
+        </label>
+          <label>
+              Password:
+              <input type="text" name="password" />
+          </label>
+        <input type="submit" value="Submit" />
+      </form>
+        <a>Sign Up</a>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
+    label: {
+        display: "inline-block",
+        width: "100%",
+    },
+
+    form: {
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
