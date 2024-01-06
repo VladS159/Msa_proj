7d67fb2dc21a26755e6878a1c7a4d50d9e20a8f1
Mnv22 <mnv22081@gmail.com>
2024-01-06 23:46:58 +0200

    bro wtf

--- mankey/my-app/src/components/CustomTabs/CustomTabs.js
+++ mankey/my-app/src/components/CustomTabs/CustomTabs.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {View, Text, Image, StyleSheet, useWindowDimensions, TextInput, Pressable} from 'react-native'
 import { Ionicons } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 
 const CustomTabs = ({currentText, onPress}) => {
 
@@ -12,6 +13,10 @@ const CustomTabs = ({currentText, onPress}) => {
             <Pressable style={styles.tab}>
                 <Ionicons name="home-outline" size={24} color="black" style={styles.icon}/>
                 <Text>Home</Text>
+            </Pressable>
+            <Pressable style={styles.tab}>
+                <AntDesign name="pluscircleo" size={24} color="black" style={styles.icon}/>
+                <Text>Add Task</Text>
             </Pressable>
             <Pressable style={styles.tab}>
                 <MaterialCommunityIcons name="calendar-blank-multiple" size={24} color="black" style={styles.icon}/>
